e7947b5c45b211eaa74b47874d1a9a58fe3478d9
omenkish <omenkish@gmail.com>
2019-09-10 13:11:40 +0100

    create google authentication with firebase

--- src/App.js
+++ src/App.js
@@ -5,21 +5,46 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="">
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <div className="">
+        <Router>
+          <Header currentUser={currentUser}/>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/shop' component={ShopPage} />
+            <Route exact path='/signin' component={SignInAndSignUpPage} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-export default ({ children, ...otherProps}) => (
-  <button className="custom-button" {...otherProps}>
+export default ({ children, isGoogleSignIn, ...otherProps}) => (
+  <button
+    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+     {...otherProps}
+  >
     {children}
   </button>
 );
