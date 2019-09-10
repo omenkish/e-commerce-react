cfe1cb14882aee3ba4b9d97fc85a691517bef0d9
omenkish <omenkish@gmail.com>
2019-09-10 22:57:39 +0100

    implement user sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends Component {
@@ -16,13 +16,19 @@ class SignIn extends Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
 
-    this.setState({
-      email: '',
-      password: ''
-    })
+    try {
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({
+        email: '',
+        password: ''
+      });
+    } catch (error) {
+      console.log('error :', error);
+    }
   }
 
   handleChange = event => {
