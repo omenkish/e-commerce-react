4d591922bbb44cf666cd7e4844104b6670e93c2d
omenkish <omenkish@gmail.com>
2019-09-25 19:08:39 +0100

    add express server to handle backend strip, move cra to client folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_29UlvfkeS5zQWFRxat3ED2bs002O3b1qHk';
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+  const onToken = async token => {
+    try {
+      await axios({
+        url: 'payment',
+        method: 'post',
+        data: {
+          amount: priceForStripe,
+          token,
+        },
+      });
+      alert('Payment successful!');
+    } catch (error) {
+      console.log('Payment error: ', JSON.parse(error));
+      alert(
+        'There was an issue with your payment. Please ensure you use the credit card details provided'
+      );
+    }
   }
   return (
     <StripeCheckout
